e3e556f011c7661eb251647f10d59e2939a806d3
chenyihui <chenyihui@nonobank.com>
2017-11-15 20:42:32 +0800

    创建项目

---
+++ java8/src/main/java/com/java8/List/Person.java
@@ -0,0 +1,30 @@
+package com.java8.List;
+
+
+/**
+ * Created by cyh on 2016-04-17.
+ */
+public  class Person { //implements Comparable<Person>
+
+    private final String name;
+    private final int age;
+
+
+    public String getName(){return name;}
+    public int getAge(){return age;}
+    public  Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public String toString() {
+        return String.format("%s-%d",name,age);
+    }
+
+  /* public int compareTo(Person other){
+       return ((Integer)(age)).compareTo(other.age);
+   }*/
+
+
+}
---
+++ java8/src/main/java/com/java8/lambda/LamdaGetNumber.java
@@ -0,0 +1,25 @@
+package com.java8.lambda;
+
+/**
+ * Created by cyh on 2016-05-01.
+ * 获取一个数字
+ */
+//@FunctionalInterface
+interface  MyNumber{
+    double getValue();
+}
+public class LamdaGetNumber {
+    public static void main(String[] args) {
+        MyNumber myNumber;
+
+        myNumber=()->123.45;
+        System.out.println("A fixed value: "+myNumber.getValue());
+
+        myNumber=()->Math.random()*10;
+        System.out.println(" A random value: "+myNumber.getValue());
+        System.out.println("Another random value: "+myNumber.getValue());
+
+        //Lambda 必须与想要实现的抽象方法兼容。类型必须一直
+        // myNumber=()->"123"; String类型与double不一直
+    }
+}

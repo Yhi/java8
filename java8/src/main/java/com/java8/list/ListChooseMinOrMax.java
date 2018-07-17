311a6ceb6934830efe55006a7c06509533f2b923
git-eason <chenyihui7318@sina.com>
2018-07-17 17:03:23 +0800

    add list hash explain

--- java8/src/main/java/com/java8/list/ListChooseMinOrMax.java
+++ java8/src/main/java/com/java8/list/ListChooseMinOrMax.java
@@ -1,6 +1,7 @@
 package com.java8.list;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,26 +10,27 @@ import java.util.Optional;
  */
 public class ListChooseMinOrMax {
     public static void main(String[] args) {
-        List<Integer> myList=new ArrayList<>();
+        List<Integer> myList = new ArrayList<>();
         myList.add(6);
         myList.add(99);
         myList.add(1);
         myList.add(333);
         myList.add(11);
 
-
-        System.out.println("min: "+ minElement(myList));
-        System.out.println("max: "+maxElement(myList));
+        System.out.println("min: " + minElement(myList));
+        System.out.println("max: " + maxElement(myList));
 
     }
+
     //找出list中最小的值
-    public static Integer minElement(List<Integer> list){
-        Optional<Integer> optMin=list.stream().min(Integer::compare);
+    public static Integer minElement(List<Integer> list) {
+        Optional<Integer> optMin = list.stream().min(Integer::compare);
         return optMin.get();
     }
+
     //找出list中最大的值
-    public static Integer maxElement(List<Integer> list){
-        Optional<Integer> optMin=list.stream().max(Integer::compare);
+    public static Integer maxElement(List<Integer> list) {
+        Optional<Integer> optMin = list.stream().max(Integer::compare);
         return optMin.get();
     }
 }
--- java8/src/main/java/com/java8/list/ListToMap.java
+++ java8/src/main/java/com/java8/list/ListToMap.java
@@ -16,32 +16,34 @@ public class ListToMap {
         listToMapJava8(addlist());
 
     }
-    public static List addlist(){
-        List list=new ArrayList<>();
+
+    public static List addlist() {
+        List list = new ArrayList<>();
         list.add("a");
         list.add(2);
         list.add(3.0);
         list.add('q');
         return list;
     }
+
     //list转Map
-    public static void listToMapJava7(List list){
-        Map<String,Object> map= new HashMap<>();
+    public static void listToMapJava7(List list) {
+        Map<String, Object> map = new HashMap<>();
         for (Object obj : list) {
             map.put(obj.toString(), obj);
         }
         for (Map.Entry<String, Object> entry : map.entrySet()) {
-            System.out.println("java7 "+"key : " + entry.getKey() + " value : " + entry.getValue());
+            System.out.println("java7 " + "key : " + entry.getKey() + " value : " + entry.getValue());
         }
 
     }
 
     //list转Map
-    public static void listToMapJava8(List list){
-        Map map= (Map) list.stream().collect(
+    public static void listToMapJava8(List list) {
+        Map map = (Map) list.stream().collect(
                 Collectors.toMap(Object::toString, Function.identity()));
-        map.forEach((k,v)->{
-            System.out.println("java8 "+"key : " + k + " value : " + v);
+        map.forEach((k, v) -> {
+            System.out.println("java8 " + "key : " + k + " value : " + v);
         });
     }
 }
